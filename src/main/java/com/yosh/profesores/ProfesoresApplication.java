be65242f4a1913cada47636f18d466fada488b6b
Andres Aguilar <andresyoshimar@gmail.com>
2018-09-20 22:14:50 -0500

    Agregando estructura del proyecto

---
+++ src/main/java/com/yosh/profesores/ProfesoresApplication.java
@@ -0,0 +1,12 @@
+package com.yosh.profesores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfesoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfesoresApplication.class, args);
+	}
+}
