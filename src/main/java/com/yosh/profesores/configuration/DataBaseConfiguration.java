4b9f6824c8146b934a5393d8f7a0dee118538ea4
Andrés Aguilar <andresyoshimar@gmail.com>
2018-09-24 21:17:19 -0500

    AAgregando clase de configuracion para hibernate

---
+++ src/main/java/com/yosh/profesores/configuration/DataBaseConfiguration.java
@@ -0,0 +1,60 @@
+package com.yosh.profesores.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DataBaseConfiguration {
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		
+		sessionFactoryBean.setDataSource(dataSource());
+		sessionFactoryBean.setPackagesToScan("com.yosh.profesores.models");
+		sessionFactoryBean.setHibernateProperties(hibernateProperties());
+		
+		return sessionFactoryBean;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/escuela?useUnicode=true&amp;useJDBCCompliantTimezoneShift=true&amp;useLegacyDatetimeCode=false&amp;serverTimezone=UTC");
+		dataSource.setUsername("test");
+		//  dataSource.setPassword("");
+		
+		return dataSource;
+	}
+	
+	public Properties hibernateProperties() {
+		Properties properties = new Properties();
+		
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		properties.put("show_sq", "true");
+		
+		return properties;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager manager = new HibernateTransactionManager();
+		
+		manager.setSessionFactory(sessionFactory().getObject());
+		
+		return manager;
+	}
+}
