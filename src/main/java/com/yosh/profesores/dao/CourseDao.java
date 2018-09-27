80cb29e1784392d3b82b82d420e46ab01520efec
Andrés Aguilar <andresyoshimar@gmail.com>
2018-09-26 20:04:08 -0500

    Agregando interfaz y DAO para Course

---
+++ src/main/java/com/yosh/profesores/dao/CourseDao.java
@@ -0,0 +1,46 @@
+package com.yosh.profesores.dao;
+
+import java.util.List;
+
+import com.yosh.profesores.models.Course;
+
+public class CourseDao extends AbstractSession implements ICourse{
+	
+	public CourseDao() {}
+
+	@Override
+	public void save(Course course) {
+		getSession().persist(course);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Course> findAll() {
+		return (List<Course>)getSession().createQuery("from Course").list();
+	}
+
+	@Override
+	public Course findById(Long idCourse) {
+		return getSession().get(Course.class, idCourse);
+	}
+
+	@Override
+	public void deleteById(Long idCourse) {
+		Course course = findById(idCourse);
+		if (course != null)
+			getSession().delete(course);
+	}
+
+	@Override
+	public void update(Course course) {
+		getSession().update(course);
+	}
+
+	@Override
+	public Course findByName(String name) {
+		return (Course)getSession().createQuery("from Course where name = :name")
+				.setParameter(":name", name)
+				.uniqueResult();
+	}
+
+}
---
+++ src/main/java/com/yosh/profesores/dao/ICourse.java
@@ -0,0 +1,21 @@
+package com.yosh.profesores.dao;
+
+import java.util.List;
+
+import com.yosh.profesores.models.Course;
+
+public interface ICourse {
+
+	void save(Course course);
+	
+	List<Course> findAll();
+	
+	Course findById(Long idCourse);
+	
+	Course findByName(String name);
+	
+	void deleteById(Long idCourse);
+	
+	void update(Course course);
+	
+}
