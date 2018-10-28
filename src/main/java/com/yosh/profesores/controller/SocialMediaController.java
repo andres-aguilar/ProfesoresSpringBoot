f9de8fcb373f85cc863f4cea500ae61233d3bce7
Andrés Aguilar <andresyoshimar@gmail.com>
2018-10-27 19:42:48 -0600

    Agregando anotacion para suprimir las advertencias del eclipse

--- src/main/java/com/yosh/profesores/controller/SocialMediaController.java
+++ src/main/java/com/yosh/profesores/controller/SocialMediaController.java
@@ -33,6 +33,7 @@ public class SocialMediaController {
 	 *  
 	 *  @return ResponseEntity<List<SocialMedia>>
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(value="/socialMedias", method=RequestMethod.GET, headers="Accept=application/json")
 	public ResponseEntity<List<SocialMedia>> getSocialMedias(@RequestParam(value="name", required=false) String name) {
 		List<SocialMedia> socialMedias = new ArrayList<>();
@@ -64,6 +65,7 @@ public class SocialMediaController {
 	 *  
 	 *  @return ResponseEntity<SocialMedia>
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(value="/socialMedias/{id}", method=RequestMethod.GET, headers="Accept=application/json")
 	public ResponseEntity<SocialMedia> getSocialMediaById(@PathVariable("id") Long id) {
 		if(id == null || id < 0) {
@@ -85,6 +87,7 @@ public class SocialMediaController {
 	 * 
 	 * @return ResponseEntity<?>
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(value="/socialMedias", method=RequestMethod.POST, headers="Accept=application/json")
 	public ResponseEntity<?> createSocialMedia(@RequestBody SocialMedia socialMedia, UriComponentsBuilder uriComponentBuilder) {
 		if (socialMedia.getName().equals(null) || socialMedia.getName().isEmpty()) {
@@ -112,6 +115,7 @@ public class SocialMediaController {
 	 *   
 	 *   @return ResponseEntity<SocialMedia>
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(value="/socialMedias/{id}", method=RequestMethod.PATCH, headers="Accept=appliucation/json")
 	public ResponseEntity<?> updateSocialMedia(@PathVariable("id") Long id, @RequestBody SocialMedia socialMedia) {
 		if(id == null || id < 0) {
@@ -140,6 +144,7 @@ public class SocialMediaController {
 	 *  
 	 *  @return ResponseEntity<?>
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(value="/socialMedias/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<?> deleteSocialMedia(@PathVariable("id") Long id) {
 		if(id == null || id < 0) {
